9b47b0da181815cf2fa4fc9b8dc5de7b78a80ae3
JoelCAI <joelargentina12345@gmail.com>
2022-04-09 00:16:38 -0300

    Agregar archivos de proyecto.

---
+++ Fechas23/Sistema.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas23
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+
+            string[] arregloFecha = new string[cantidad];
+
+            Validador.Bienvenida();
+
+            Validador.FechaValida(ref arregloFecha);
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+
+        public static int cantidad = 1;
+
+
+    }
+}
---
+++ Fechas23/Validador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas23
+{
+    public class Validador
+    {
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+        public const int cantidad = 1;
+        public static void FechaValida(ref string[] arregloFecha)
+        {
+            /* "05/05/2009" */
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                string fechaUno;
+
+                do
+                {
+
+                    Console.WriteLine(" \n Ingrese la fecha " + (i + 1).ToString() + ": ");
+                    Console.WriteLine(" \n El formato válido es: *dd/mm/aaaa hh:mm:ss* ");
+
+                    fechaUno = Console.ReadLine();
+
+                    if (fechaUno == "")
+                    {
+                        Console.Clear();
+                        Console.WriteLine(" \n Ingrese un valor que no sea vacío por favor");
+                    }
+
+                } while (fechaUno == "");
+
+                arregloFecha[i] = fechaUno;
+
+            }
+
+            string[] dateStrings = arregloFecha;
+
+            DateTime dateValue;
+
+            foreach (string dateString in dateStrings)
+            {
+                if (DateTime.TryParse(dateString, out dateValue))
+                    Console.WriteLine(" \n Fecha ingresada válida " +
+                                      " \n Usted escribió *{0}* " +
+                                      " \n Por lo tanto el sistema lo convirtio correctamente a: *{1}* ", dateString,
+                                      dateValue, dateValue.Kind);
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine(" \n Fecha ingresada inválida " +
+                                      " \n Ingrese un formato dd/mm/aaaa hh:mm:ss ");
+                }
+                    
+
+            }
+
+        }
+
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+    }
+}
